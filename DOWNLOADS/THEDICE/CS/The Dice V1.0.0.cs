92228b2e8f7dc727b04b9b839d6cc0163043c1ea
fsforward <89132362+fsforward@users.noreply.github.com>
2022-06-23 20:46:39 +0200

    e

---
+++ DOWNLOADS/THEDICE/CS/The Dice V1.0.0.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace V1._0._0
+{
+    class thedice
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Info();
+        }
+
+        static void Info()
+        {
+            Console.WriteLine("Project:\tThe Dice");
+            Console.WriteLine("Version:\t1.0.0");
+            Console.WriteLine("Made by:\tfsForward");
+            
+            Console.WriteLine("\nCTRL + C to exit");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            RollDice();
+        }
+
+        static void RollDice()
+        {
+            Console.Clear();
+
+            Random dice = new Random();
+            Console.WriteLine(dice.Next(1, 7));
+            Console.Write("Press any key to roll The Dice again...");
+            Console.ReadKey();
+            RollDice();
+        }
+    }
+}
---
+++ DOWNLOADS/THEDICE/CS/The Dice V1.1.0.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace V1._1._0
+{
+    class thedice
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Info();
+        }
+
+        static void Info()
+        {
+            Console.WriteLine("Project:\tThe Dice");
+            Console.WriteLine("Version:\t1.1.0");
+            Console.WriteLine("Made by:\tfsForward");
+            
+            Console.WriteLine("\nCTRL + C to exit");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            RollDice();
+        }
+
+        static void RollDice()
+        {
+            int rolls = 0;
+            while (true)
+            {
+                Console.Clear();
+
+                Random dice = new Random();
+                rolls++;
+                Console.WriteLine($"You rolled a: {dice.Next(1, 7)}");
+                Console.WriteLine($"Dice rolled: {rolls} times");
+
+                Console.Write("\nPress any key to roll The Dice again...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
