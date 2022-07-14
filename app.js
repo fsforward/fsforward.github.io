896a7f5e4a2a29af2d1bbe8eba623ad7b9be35b7
fsforward <fsforward@hotmail.com>
2022-07-14 08:23:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ for (let i = 0; i < list.length; i++) {
             card[k].classList.remove("active");
             card[k].classList.add("hide");
 
-            if (card[k].getAttribute("data-item") == dataFilter ||  dataFilter == "all") {
+            if (card[k].getAttribute("data-item") == dataFilter || dataFilter == "all") {
                 card[k].classList.remove("hide");
                 card[k].classList.add("active");
             }
