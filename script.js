34fc9e8114810c7ad3fb2f570fc2361e2d8dd140
fsforward <fsforward@hotmail.com>
2023-01-22 12:08:38 -0800

    Fixes

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 function filterImages() {
-  var input = document.getElementById("search-input");
-  var filter = input.value.toUpperCase();
-  var images = document.getElementsByClassName("image");
+  const searchValue = document.getElementById("search-input").value.toLowerCase();
+  const images = document.getElementsByClassName("image");
 
-  for (var i = 0; i < images.length; i++) {
-    var imageName = images[i].getAttribute("data-name");
-    if (imageName.toUpperCase().indexOf(filter) > -1) {
-      images[i].style.display = "";
-    } else {
+  for (let i = 0; i < images.length; i++) {
+    const name = images[i].getAttribute("data-name").toLowerCase();
+    if (name.indexOf(searchValue) === -1) {
+      // image does not match search input, hide it
       images[i].style.display = "none";
+    } else {
+      // image matches search input, show it
+      images[i].style.display = "block";
+      // get position of visible images
+      const rect = images[i].getBoundingClientRect();
+      // set the position of hidden images to match the visible images
+      for (let j = 0; j < images.length; j++) {
+        if (images[j].style.display === "none") {
+          images[j].style.top = rect.top + "px";
+          images[j].style.left = rect.left + "px";
+          break;
+        }
+      }
     }
   }
 }
