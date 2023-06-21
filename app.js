474171188df92fb609956b9b89bbc46eea51595c
fsforward <89132362+fsforward@users.noreply.github.com>
2023-06-21 22:25:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
-  // Check the screen width on initial page load
-  checkScreenWidth();
+  // Check if not on desktop on initial page load
+  checkIfNotDesktop();
 
-  // Get references to the necessary elements
-  var searchForm = document.querySelector('.search-form');
-  var mainSection = document.querySelector('.main-section');
-  var searchSection = document.querySelector('.search-section');
-  var cardContainers = document.querySelectorAll('.search-section .card-container');
-  var cards = document.querySelectorAll('.search-section .mod-card');
+  // Rest of the code...
 
-  // Add event listener to the search form
-  searchForm.addEventListener('input', function(event) {
-    var searchQuery = event.target.value.trim().toLowerCase();
-
-    // Hide or show the main and search sections based on the search query
-    if (searchQuery !== '') {
-      mainSection.style.display = 'none';
-      searchSection.style.display = 'block';
-
-      // Filter and display the matching cards
-      cards.forEach(function(card) {
-        var cardTitle = card.querySelector('h3').textContent.toLowerCase();
-        if (cardTitle.includes(searchQuery)) {
-          card.style.display = 'block';
-        } else {
-          card.style.display = 'none';
-        }
-      });
-    } else {
-      mainSection.style.display = 'block';
-      searchSection.style.display = 'none';
-    }
-  });
 });
 
 window.addEventListener('resize', function() {
-  // Check the screen width on window resize
-  checkScreenWidth();
+  // Check if not on desktop on window resize
+  checkIfNotDesktop();
 });
 
-function checkScreenWidth() {
-  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+function checkIfNotDesktop() {
+  var isNotDesktop = !window.matchMedia('(hover: hover)').matches;
   var urlParams = new URLSearchParams(window.location.search);
   var debugMode = urlParams.has('debug-mode');
   var debugWarning = document.getElementById('debugWarning');
 
-  if (screenWidth < 1000 && !debugMode) {
-    // Redirect to denied page if screen width is less than 600px and not in debug mode
+  if (isNotDesktop && !debugMode) {
+    // Redirect to denied page if not on desktop and not in debug mode
     if (!window.location.href.includes('access-denied.html')) {
       window.location.href = 'access-denied.html';
     }
   } else {
-    // Redirect to index page if on denied page and screen width is greater than or equal to 600px, or in debug mode
+    // Redirect to index page if on denied page and on desktop, or in debug mode
     if (window.location.href.includes('access-denied.html')) {
       window.location.href = 'index.html';
     }
@@ -65,6 +37,7 @@ function checkScreenWidth() {
     }
   }
 }
+
 
 
 function openPopup(popupId) {
