53a9c23b48988e7236b33cb338756db870b5d45a
fsForward <fsforward@hotmail.com>
2023-08-16 14:01:50 +0200

    Update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const isDesktop = !window.matchMedia('(pointer: fine)').matches;
-
+    
     const links = document.querySelectorAll(".sidebar ul li a");
     const sections = document.querySelectorAll("main section");
     const toggleButton = document.querySelector(".toggle-button");
     const sidebar = document.querySelector(".sidebar");
     const logo = document.querySelector(".logo");
+    const isDesktop = window.matchMedia('(pointer: fine)').matches;
 
     links.forEach(link => {
         link.addEventListener("click", function (event) {
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             this.classList.add("active");
             document.querySelector(`.${targetSection}`).classList.add("shown");
 
-            if (isDesktop) {
+            if (!isDesktop) {
                 // Collapse the sidebar
                 sidebar.classList.add("collapsed");
                 logo.classList.add("hidden");
@@ -30,9 +30,21 @@ document.addEventListener("DOMContentLoaded", function () {
         sidebar.classList.toggle("collapsed");
         logo.classList.toggle("hidden");
     });
+    const lockIcons = document.querySelectorAll("i.bx.bxs-lock");
 
-    if (isDesktop) {
+    if (!isDesktop) {
         sidebar.classList.add("collapsed");
         logo.classList.add("hidden");
+    
+        lockIcons.forEach(lockIcon => {
+            const link = lockIcon.parentElement; // Get the <a> element containing the lock icon
+            link.removeAttribute("href"); // Remove the href attribute to disable the link
+            link.style.pointerEvents = "none"; // Disable pointer events on the link
+        });
+    } else {
+        lockIcons.forEach(lockIcon => {
+            lockIcon.style.display = "none"; // Hide each lock icon
+        });
     }
+    
 });
